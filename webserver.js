1268b83a155d2872322078eca662fd054fb00f9c
James McDaniel <icathaid@gmail.com>
2018-04-29 15:26:34 -0700

    testing web connection

--- webserver.js
+++ webserver.js
@@ -2,7 +2,10 @@
 
 var http = require('http').createServer(handler);
 var fs = require('fs');
-var io = require('socket.io')(http)
+var io = require('socket.io')(http);
+var Gpio = require('onoff').Gpio;
+var RED = new Gpio(26, 'out');
+var pushButton = new Gpio(17, 'out', 'both');
 
 http.listen(8080);
 
@@ -20,10 +23,27 @@ function handler (req, res) { //create server
   
   io.sockets.on('connection', function(socket){
     var lightValue = 0;
+    pushButton.watch(function(err, value){
+      if(err){
+        console.error('There was an error', err);
+        return;
+      };
+      lightValue = value;
+      socket.emit('light', lightValue);
+
+    });
     socket.on('light', function(data){
       lightValue = data;
-      if (lightValue) {
-        console.log(lightValue);
+      if (lightValue != RED.readSync()) {
+        RED.writeSync(lightValue);
+        // console.log(lightValue);
       }
     });
+  });
+
+  process.on('SIGINT', function(){
+    RED.writeSync(0);
+    RED.unexport();
+    pushButton.unexport();
+    process.exit();
   });
