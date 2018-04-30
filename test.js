7b1efafd1f268d4e326ee1ec7836ad93ac5be08d
James McDaniel <icathaid@gmail.com>
2018-04-29 18:08:57 -0700

    added vars

--- test.js
+++ test.js
@@ -3,8 +3,16 @@
 console.log('Welcome');
 var Gpio = require('onoff').Gpio;
 var ONE = new Gpio(21, 'out');
+var TWO = new Gpio(18, 'in');
+var THREE = new Gpio(26, 'in');
+var FOUR = new Gpio(20, 'in');
+var FIVE = new Gpio(19, 'in');
+var SIX = new Gpio(16, 'in');
+var SEVEN = new Gpio(13, 'in');
+var EIGHT = new Gpio(12, 'in');
 var buttonDown = new Gpio(4, 'in', 'falling');
 var buttonUp = new Gpio(17, 'in', 'rising');
+var county = 0;
 
 buttonDown.watch(function(err, value){
     if(err){
