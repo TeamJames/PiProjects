98225c78de49be440b85a22a9f7ac067129213df
James McDaniel <icathaid@gmail.com>
2018-10-26 16:59:59 -0700

    reversed button

--- growBox/water/water.js
+++ growBox/water/water.js
@@ -5,7 +5,7 @@
 var Gpio = require('onoff').Gpio;
 // var buttonUp = new Gpio(4, 'in', 'rising');
 var green = new Gpio(4, 'out');
-var buttonDown = new Gpio(26, 'in', 'falling');
+var buttonDown = new Gpio(26, 'out', 'falling');
 
 let counter = 0;
 
@@ -15,25 +15,13 @@ buttonDown.watch(function(err, value){
         return;
     };
     counter +=1;
-    console.log('button falling value:     ', value);
     console.log('Counter: ', counter);
-    // RED.writeSync(0);
     green.writeSync(1);
 });
-// buttonDown.watch(function(err, value){
-//     if(err){
-//         console.error('There was an error', err);
-//         return;
-//     }
-//     RED.writeSync(1);
-//     GREEN.writeSync(0);
-// });
+
 function unexportOnClose(){
-    // RED.writeSync(0);
-    // RED.unexport();
-    green.writeSync(0);
-    green.unexport();
-    // buttonUp.unexport();
+    green-.writeSync(0);
+    green-.unexport();
     buttonDown.unexport();
 };
 
