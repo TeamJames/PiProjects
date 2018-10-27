f293352ac4679f8af543bc9e283a10353a2d57c6
James McDaniel <icathaid@gmail.com>
2018-10-26 18:30:14 -0700

    STOP WORKING ON MASTER JAMES

--- growBox/water/water.js
+++ growBox/water/water.js
@@ -6,14 +6,14 @@ const Gpio = require('onoff').Gpio;
 
 const thirstyPlants = new Gpio(4, 'out');
 const mockedWaterPump = new Gpio(27, 'out');
+const mockedDrainValve = new Gpio(6, 'out');
 const mockedMoistureSensor = new Gpio(26, 'in', 'rising');
 
 console.log('growBox started on port 420');
 
 mockedMoistureSensor.watch(function(err, value){
     if(err){
-        console.error('There was an error', err);
-        return;
+        return console.error('There was an error', err);;
     };
     console.log('Plants are thirsty!');
     thirstyPlants.writeSync(1);
@@ -36,8 +36,18 @@ const wateredPlants = function(){
     console.log('Plants are well hydrated');
     const watered = function(){
         thirstyPlants.writeSync(0);
+        const startDrainingWaste = function(){
+            console.log('opening drain valve');
+            mockedDrainValve.writeSync(1);
+            const stopDrainingWaste = function(){
+                console.log('closing drain valve');
+                mockedDrainValve.writeSync(0);
+            };
+            setTimeout(stopDrainingWaste, 3000);
+        };
+        setTimeout(startDrainingWaste, 3000);
     };
-    setTimeout(watered, 500);
+    setTimeout(watered, 1000);
     thirstyPlants.writeSync(0);
     mockedWaterPump.writeSync(0);
     console.log('growBox ready');
