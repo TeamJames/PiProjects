753ff3333291ecb7cadf894a584a7879bd761cb4
James McDaniel <icathaid@gmail.com>
2018-10-26 19:22:04 -0700

    testing modules

---
+++ growBox/test/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default Class Test {
+  speak(){
+    return console.log('test module imported correclty');
+  }
+}
--- growBox/water/water.js
+++ growBox/water/water.js
@@ -2,12 +2,17 @@
 
 const Gpio = require('onoff').Gpio;
 
+const Test = require('../test/test.js');
+
 const thirstyPlants = new Gpio(4, 'out');
 const mockedWaterPump = new Gpio(27, 'out');
 const mockedDrainValve = new Gpio(6, 'out');
 const mockedMoistureSensor = new Gpio(26, 'in', 'rising');
 
 console.log('growBox started on port 420');
+let cheech = new Test;
+console.log(cheech.speak);
+
 
 mockedMoistureSensor.watch(function(err, value){
     if(err){
