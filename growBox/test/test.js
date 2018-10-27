2c89a3e71714e04dc98faf5ca082db86086e681b
James McDaniel <icathaid@gmail.com>
2018-10-27 02:10:04 -0700

    modularizing

--- growBox/test/test.js
+++ growBox/test/test.js
@@ -1,26 +1,26 @@
 'use strict';
 
-
+const Gpio = require('onoff').Gpio;
 const waterPumpLED = new Gpio(20, 'out');
 
 module.exports = {
-  testy(){
-    return console.log('testy');
-  }
-  waterStart() {
-    console.log('water pump on');
-    waterPumpLED.writeSync(1);
-    setTimeout(waterStop, 2000);
-}
-waterStop(){
-    console.log('water pump off');
-    thirstyPlants.writeSync(0);
-    console.log('plants are well hydrated');
-    waterPumpLED.writeSync(0);
-    reservoir +=1;
-    if(reservoir === 10){
-        return drainWaste();
-    };
-    status();
-}
+    testy() {
+        return console.log('testy');
+    },
+    waterStart() {
+        console.log('water pump on');
+        waterPumpLED.writeSync(1);
+        setTimeout(waterStop, 2000);
+    },
+    waterStop() {
+        console.log('water pump off');
+        thirstyPlants.writeSync(0);
+        console.log('plants are well hydrated');
+        waterPumpLED.writeSync(0);
+        reservoir += 1;
+        if (reservoir === 10) {
+            return drainWaste();
+        };
+        status();
+    }
 };
