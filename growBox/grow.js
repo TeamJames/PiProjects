512d7b1978d2e47ec2180d624861a20209e0b294
James McDaniel <icathaid@gmail.com>
2018-10-26 20:26:31 -0700

    ready for testing

--- growBox/grow.js
+++ growBox/grow.js
@@ -9,15 +9,59 @@ const serverStatus = new Gpio(21, 'out');
 const thirstyPlants = new Gpio(12, 'out');
 const mockedWaterPump = new Gpio(20, 'out');
 const mockedDrainValve = new Gpio(16, 'out');
+
 const startButton = new Gpio(19, 'in', 'rising');
-const mockedMoistureSensor = new Gpio(13, 'in', 'rising');
+const mockedMoistureSensor = new Gpio(5, 'in', 'rising');
+const mockedDrainValveRelay = new Gpio(13, 'in', 'rising');
 const stopButton = new Gpio(26, 'in', 'rising');
+
+startButton.watch(function(err, value){
+  if(err){
+    return console.error(err);
+  }
+  water.reset();
+  serverStatus.writeSync(1);
+});
 
 mockedMoistureSensor.watch(function(err, value){
     if(err){
-        return console.error('There was an error', err);;
-    };
+        return console.error(err);
+    }
     console.log('Plants are thirsty!');
     thirstyPlants.writeSync(1);
     water.waterStart();
 });
+
+mockedDrainValveRelay.watch(function(err, value){
+  if(err){
+    return console.error(err);
+  }
+  console.log('Drain valve sensor indicates waste reservoir is full');
+  water.startDrainingWaste();
+});
+
+stopButton.watch(function(err, value){
+  if(err){
+    return console.error(err);
+  }
+  console.log('Shutting down growBox');
+  unexportOnClose();
+});
+
+const unexportOnClose = function(){
+    thirstyPlants.writeSync(0);
+    mockedWaterPump.writeSync(0);
+    mockedDrainValve.writeSync(0);
+    serverStatus.writeSync(0);
+    serverStatus.writesync(0);
+    thirstyPlants.unexport();
+    mockedWaterPump.unexport();
+    mockedDrainValve.unexport();
+    mockedMoistureSensor.unexport();
+    startButton.unexport();
+    stopButton.unexport();
+    mockedDrainValveRelay.unexport();
+    console.log('growbox shut down');
+};
+
+process.on('SIGINT', unexportOnClose);
--- growBox/water/water.js
+++ growBox/water/water.js
@@ -1,20 +1,7 @@
 'use strict';
 
-// const Gpio = require('onoff').Gpio;
-
-// const test = require('../test/test.js');
-
-// const serverStatus = new Gpio(21, 'out');
-// const thirstyPlants = new Gpio(12, 'out');
-// const mockedWaterPump = new Gpio(20, 'out');
-// const mockedDrainValve = new Gpio(16, 'out');
-// const startButton = new Gpio(19, 'in', 'rising');
-// const mockedMoistureSensor = new Gpio(13, 'in', 'rising');
-// const stopButton = new Gpio(26, 'in', 'rising');
-
-
+class Water {
 console.log('growBox started on port 420');
-test();
 
 mockedMoistureSensor.watch(function(err, value){
     if(err){
@@ -39,29 +26,22 @@ const waterStop = function(){
 const startDrainingWaste = function(){
     console.log('opening drain valve');
     mockedDrainValve.writeSync(1);
-    const stopDrainingWaste = function(){
-      console.log('closing drain valve');
-      mockedDrainValve.writeSync(0);
-    };
     setTimeout(stopDrainingWaste, 3000);
 };
 
+const stopDrainingWaste = function(){
+    console.log('closing drain valve');
+    mockedDrainValve.writeSync(0);
+};
 
-
-function shutDown() {
+function reset() {
     thirstyPlants.writeSync(0);
     mockedWaterPump.writeSync(0);
     mockedDrainValve.writeSync(0);
     console.log('growBox ready');
 };
 
-const unexportOnClose = function(){
-    thirstyPlants.writeSync(0);
-    mockedWaterPump.writeSync(0);
-    thirstyPlants.unexport();
-    mockedWaterPump.unexport();
-    mockedMoistureSensor.unexport();
-    console.log('growbox shut down');
 };
 
-process.on('SIGINT', unexportOnClose);
+module.exports = Water;
+
